5cd05bb94bc4d37044c819db9da83849b1d232e7
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-06-04 19:41:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="discord_i18n",
     author="Clutter Development",
     version="1.0.1",
     license="MIT",
     description="A simple I18N handler for Discord bots.",
-    install_requires=requirements,
     python_requires=">=3.10",
     py_modules=["discord_i18n"],
     packages=["discord_i18n"],
